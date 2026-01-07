43202a5b9335c87193e5277abe6d7fec3a043521
elven <elven@Damian.izzigo.tv>
2026-01-06 23:47:38 -0600

    Registro api configuracion inicial

---
+++ src/test/java/com/registroAPi/resgistroApi/ResgistroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registroAPi.resgistroApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResgistroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
